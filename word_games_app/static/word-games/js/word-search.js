e94eb7d3297830f2d3e810e9ec74821b4ce29c9b
Thomas Kruithof <tkruithof95@gmail.com>
2024-06-21 17:49:18 +0200

    worked on search

--- word_games_app/static/word-games/js/word-search.js
+++ word_games_app/static/word-games/js/word-search.js
@@ -23,8 +23,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // set game state and timer
     const gameState = new GameState("wordSearch", 0, playBtn, loadingSpinner, userId, endContainer);
+    let gameStarted = false;
 
-    const timer = new Timer(90, timerDisplay, timerEnd);
+    //const timer = new Timer(90, timerDisplay, timerEnd);
 
     const letterDice = [
         ['r', 'i', 'f', 'o', 'b', 'x'],
@@ -53,14 +54,80 @@ document.addEventListener("DOMContentLoaded", () => {
             const randomLetter = letter[Math.floor(Math.random() * letter.length)];
             lettersDisplay[index].innerText = randomLetter;
             allowedLetters.push(randomLetter);
+            lettersDisplay[index].innerText = randomLetter;
         });
     }
 
     // get array of unique letters and count
-    function getAllowedLettersCount() {
-        const allowedLettersCount = allowedLetters.map(letter => {
-            
-        });
+    function getAllowedLettersCount(letters) {
+        const lettersCount = letters.reduce((acc ,letter) => {
+            acc[letter] = (acc[letter] || 0) + 1;
+            return acc;
+        }, {});
+
+        return lettersCount;
     }
+
+    // check if letter allowed
+    function checkAllowedLetter(letter, lettersCount) {
+        if (letter in lettersCount) {
+            if (lettersCount[letter] > 0) {
+                lettersCount -= 1;
+                return lettersCount;
+            }
+        }
+        return null;
+    }
+
+    // display letters on screen 
+    gameState.playBtn.addEventListener("click", () => {
+        if (gameState.ended && !gameState.resendData) {
+            location.reload();
+            return;
+        }
+        else if (gameState.resendData) {
+            gameState.resendData = false;
+            gameState.gameOver();
+            return;
+        }
+
+        if (!gameStarted) {
+            gameStarted = true;
+            getLetters();
+            console.log(allowedLetters)
+            const allowedLettersCount = getAllowedLettersCount(allowedLetters);
+            console.log(allowedLettersCount);
+
+            textInput.disabled = false;
+            // start timer
+            gameState.playBtn.disabled = true;
+        }
+        //const wordStr = textInput.value;
+    });
+
+    // start timer 
+
+    // listen for input, check if letter is allowed
+    textInput.addEventListener("input", () => {
+
+        const value = textInput.value;
+        const lastChar = textInput.value[textInput.value.length - 1];
+
+        if (!/^[a-zA-Z]*$/.test(lastChar)) {
+            textInput.value = value.slice(0, -1);
+        }
+        // check if letter is allowed
+
+        if (textInput.value.length > 3) {
+            gameState.playBtn.disabled = false;
+        }
+        else {
+            gameState.playBtn.disabled = true;
+        }
+    })
+
+    // send words to server on game over
+
+    // calculate points
 
 });
